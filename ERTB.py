22022c438b32e25254a8ccf1e13c468232008f60
flionic <bionic.mods@gmail.com>
2024-08-07 13:52:08 +0300

    Update GPT version [2]

--- ERTB.py
+++ ERTB.py
@@ -1353,7 +1353,7 @@ def gpt_meme(text):
 
 def gpt35_request(text):
     response = client.chat.completions.create(
-        model="gpt-4o-2024-08-06",
+        model="gpt-3.5-turbo-0125",
         messages=[
             {
                 "role": "system",
@@ -1456,7 +1456,7 @@ X - стоимость посылки в евро
 def gpt_alexa():
     response = client.chat.completions.create(
         # model="gpt-4-turbo-2024-04-09",
-        model="gpt-4o-2024-08-06",
+        model="gpt-3.5-turbo-0125",
         messages=[
             # {
             #     "role": "user",
